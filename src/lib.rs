412ac3f15d6262de1950656761f77358bd4ecba9
John Jorgensen <john@2gn.com>
2017-11-13 17:58:27 -0500

    add more docs, and delay_ms function

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,39 @@
-#![crate_name = "avr-delay"]
+#![feature(asm)]
+
+#![crate_name = "avr_delay"]
+
 /// This library is intended to provide a busy-wait delay
 /// similar to the one provided by the arduino c++ utilities
 /// If you need accurate time keeping you should consider a
 /// hardware timer.
 
-#![feature(asm)]
+// This library does all of the busy-wait loop in rust.
+// The accuracy of time passed is highly dependent on the output
+// of llvm.
+//
+// The arduino library takes the approach of packing a lot of the
+// loop cycles into asm, where clocks can be counted, and the
+// outer loops add less to the inaccuracy.
 
+// This is probably not the right way to handle CPU speed.
+// Arduino runs at 16MHZ
+const MCU_SPEED: i32 = 16000000;
+
+/// Internal function to implement a variable busy-wait loop.
+/// # Arguements
+/// * 'count' - an i32, the number of times to cycle the loop.
 fn _delay(count: i32) {
-    /// Internal function to implement a variable busy-wait loop.
-    /// # Arguements
-    /// * 'count' - an i32, the number of times to cycle the loop.
     for _ in 0..count {
         // needed to keep the loop from being optimized away
         unsafe {asm!("" :::: "volatile")}
     }
 }
 
+pub fn delay_ms(ms: i32) {
+    let lpms = MCU_SPEED / 20000;
+    let dly_cnt = lpms * ms;
+    _delay(dly_cnt);
+}
 
 #[cfg(test)]
 mod tests {
