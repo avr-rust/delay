8958c59442ff2d63b4fddbb372ab22ec79cb2644
John Jorgensen <jjorgensen@hsdp.io>
2017-11-13 14:01:33 -0500

    Add the base busy-wait loop.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
+#![crate_name = "avr-delay"]
+/// This library is intended to provide a busy-wait delay
+/// similar to the one provided by the arduino c++ utilities
+/// If you need accurate time keeping you should consider a
+/// timer.
+
+#![feature(asm)]
+
+fn _delay(count: i32) {
+    /// Internal function to implement a variable busy-wait loop.
+    /// # Arguements
+    /// * 'count' - an i32, the number of times to cycle the loop.
+    for _ in 0..count {
+        // needed to keep the loop from being optimized away
+        unsafe {asm!("" :::: "volatile")}
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
     #[test]
@@ -5,3 +24,4 @@ mod tests {
         assert_eq!(2 + 2, 4);
     }
 }
+
