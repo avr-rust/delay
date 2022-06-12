29adefff19f865ba7ee475612f4e3cffc5298e6c
Geert Stappers <stappers@stappers.it>
2022-06-12 18:56:47 +0200

    white space change by `cargo fmt`

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
 #![feature(asm_experimental_arch)]
-
 #![no_std]
-
 #![crate_name = "avr_delay"]
 
 use core::arch::asm;
@@ -31,7 +29,7 @@ pub fn delay(count: u64) {
     // Our asm busy-wait takes a 16 bit word as an argument,
     // so the max number of loops is 2^16
     let outer_count = count / 65536;
-    let last_count = ((count % 65536)+1) as u16;
+    let last_count = ((count % 65536) + 1) as u16;
     for _ in 0..outer_count {
         // Each loop through should be 4 cycles.
         let zero = 0u16;
@@ -77,4 +75,3 @@ mod tests {
         assert_eq!(2 + 2, 4);
     }
 }
-
